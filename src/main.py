35a1e670d80fbad20999534bd7d22f176f44ed29
Owen <oo310@kent.ac.uk>
2019-03-02 20:11:36 +0000

    Add basic file loading/json parsing

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("greetings")
+import sys
+import json
+
+parsed = []
+for i in range(1, 4):
+    with open(sys.argv[i]) as f:
+        parsed.append(json.load(f))
+
+repository = parsed[0]
+initial = parsed[1]
+constraints = parsed[2]
+
+print(repository)
+print(initial)
+print(constraints)
